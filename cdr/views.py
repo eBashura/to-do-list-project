ce20f1563c2d559596374e255389366ea3b43ace
Evgeny <evgenybashura@gmail.com>
2023-09-27 17:03:09 +0300

    initial_commit

---
+++ cdr/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Task
+
+
+class TaskList(ListView):
+    model = Task
+
+
